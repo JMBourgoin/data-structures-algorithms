7069fc6cc995bef4ec87e8fe7d72fbb6ba3d2776
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-11 12:27:38 -0700

    add climb stairs fib

--- tabulation_project/lib/problems.js
+++ tabulation_project/lib/problems.js
@@ -110,3 +110,14 @@ module.exports = {
     maxNonAdjacentSum,
     minChange
 };
+
+var climbStairs = function(n) {
+    table = new Array(n + 1);
+     table[0] = 1;
+     table[1] = 1;
+     for(let i = 2; i <= n; i++){
+         table[i] = table[i - 1] + table[i - 2];
+     }
+     return table[table.length - 1]
+     };
+ 
