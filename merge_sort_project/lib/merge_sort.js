ef3e820d1cdd24ecc5edb6af0a1791ebfcc9dfac
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-21 12:09:32 -0400

    add merge sort solution

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,27 @@
 function merge(array1, array2) {
-
+let newArr = [];
+    while(array1.length > 0 && array2.length > 0){
+        if(array1[0] < array2[0]){
+            let ele = array1.shift();
+            newArr.push(ele);
+        } else {
+            let ele = array2.shift();
+            newArr.push(ele);
+        }
+    }
+    return newArr.concat(array1).concat(array2);
 }
 
 function mergeSort(array) {
+    if(array === undefined) return [];
+    if(array.length <= 1) return array;
 
+    const pivot = Math.floor(array.length / 2);
+    const left = array.slice(0, pivot);
+    const right = array.slice(pivot);
+    const leftSorted = mergeSort(left);
+    const rightSorted = mergeSort(right);
+    merge(leftSorted, rightSorted);
 }
 
 module.exports = {
