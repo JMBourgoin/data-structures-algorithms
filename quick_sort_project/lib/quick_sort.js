57c1cbae781cf656deca35eb4c611e0dd7d5c533
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-26 13:08:39 -0400

    solved quicksort

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,7 +1,15 @@
 function quickSort(array) {
+if(array.length <= 1) return array;
+let pivot = array[0];
+let left = array.slice(1).filter(ele => {
+    return ele <= pivot;
+});
+let right = array.slice(1).filter(ele => {
+    return ele > pivot;
+});
 
+return quickSort(left).concat(pivot).concat(quickSort(right));
 }
-
 
 module.exports = {
     quickSort
