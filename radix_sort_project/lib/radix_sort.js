ce24c3682ded585302359f9d2a64596c97d5adb2
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-26 13:47:37 -0400

    solved radix sort

--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,4 +1,27 @@
+const getDigit = (num, pos) => {
+   return Math.floor(Math.abs(num) / Math.pow(10, pos) % 10);
+};
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+const getMax = arr => {
+    let maxDigits = 0;
+    for(let i = 0; i < arr.length; i++){
+        maxDigits = Math.max(maxDigits, getIntLength(arr[i]));
+    }
+    return maxDigits;
+};
+
 function radixSort(arr) {
+    if(!Array.isArray(arr)) return null;
+    let maxDigits = getMax(arr);
+    for(let k = 0; k < maxDigits; k++){
+        let buckets = Array.from({length: 10}, () => []);
+        for(let i = 0; i < arr.length; i++){
+            let digit = getDigit(arr[i], k);
+            buckets[digit].push(arr[i]);
+        }
+        arr = [].concat(...buckets);
+    }
+    return arr;
 
 }
 
