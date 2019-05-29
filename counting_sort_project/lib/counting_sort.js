99acc9afbc70ee9949d7426d43bfa183aedfe8b1
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-29 13:19:26 -0400

    incorrect solution 1

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,18 @@
 function countingSort(arr, max) {
-
+    const countArr = new Array(max + 1).fill(0);
+    for(let k = 0; k <= max; k ++){
+        countArr[arr[k]] += 1;
+    }
+    const retArr = [];
+    for(let i = 0; i <= arr.length; i ++){
+        if(countArr[i] > 0){
+            let numTimes = countArr[i];
+            for(let j = numTimes; j > 0; j--){
+                retArr.push(i);
+            }
+        }
+    }
+    return retArr;
 }
 
 
