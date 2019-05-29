f0b3b95c3f698a96d13b98a30332d1c641248f7d
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-29 14:09:54 -0400

    add binary search boolean solution

--- binary_search_project/lib/binary_search.js
+++ binary_search_project/lib/binary_search.js
@@ -1,5 +1,16 @@
 function binarySearch(array, target) {
+    if(array.length === 0) return false;
+    let midIndx = Math.floor(array.length / 2);
+    let mid = array[midIndx];
+    if(mid === target) return true;
 
+    const leftSide = array.slice(0, midIndx);
+    const rightSide = array.slice(midIndx + 1);
+    if(target < mid){
+       return binarySearch(leftSide, target);
+    } else {
+       return binarySearch(rightSide, target);
+    }
 }
 
 function binarySearchIndex(array, target) {
