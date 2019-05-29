0cc9ab55bb6081b0b07967a03ef183b56dba1099
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-29 14:14:28 -0400

    add binary search index

--- binary_search_project/lib/binary_search.js
+++ binary_search_project/lib/binary_search.js
@@ -14,8 +14,22 @@ function binarySearch(array, target) {
 }
 
 function binarySearchIndex(array, target) {
+    if (array.length === 0) return -1;
+    let midIndx = Math.floor(array.length / 2);
+    let mid = array[midIndx];
+    if (mid === target) return midIndx;
 
+    const leftSide = array.slice(0, midIndx);
+    const rightSide = array.slice(midIndx + 1);
+    if (target < mid) {
+        return binarySearchIndex(leftSide, target);
+    } else {
+        let temp = binarySearchIndex(rightSide, target);
+        return temp === -1 ? -1 : (temp + midIndx + 1);
+    }
 }
+
+
 
 
 module.exports = {
