35305ce9198ec2141c2d51a52c7a0ccacd1cd773
Jason Bourgoin <jason@jmbourgoin.com>
2019-06-17 00:21:27 -0400

    correct answer skyline question

--- leetCode/Skyline.js
+++ leetCode/Skyline.js
@@ -4,8 +4,9 @@ var maxIncreaseKeepingSkyline = function (grid) {
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid.length; j++) {
-            row_maxes[i] = Math.max(row_maxes[i], grid[i][j]);
+            row_maxes[i] = Math.max(row_maxes[i], grid[j][i]);
             column_maxes[j] = Math.max(column_maxes[j], grid[j][i]);
+
         }
     }
 
@@ -20,3 +21,5 @@ var maxIncreaseKeepingSkyline = function (grid) {
     return ans;
 
 };
+
+maxIncreaseKeepingSkyline([[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]);
